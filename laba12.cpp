cedd2e9e594a14d4f4eea13cc1d5fd0c319e8453
rudolf43663 <rudolfgerkulesov@gmail.com>
2025-01-17 14:48:26 +0300

    laba12.cpp

--- laba12.cpp
+++ laba12.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <cctype>
-
 using namespace std;
 
 int main() {
     ifstream file("text.txt");
     if (!file) {
         cout << "Ошибка при открытии файла!" << endl;
-        return 1;
-    }
+        return 1;}
 
     int letterFrequency[26] = {0};
     int pairFrequency[26][26] = {0};
     char prevChar = '\0';
     char currentChar;
-
     while (file.get(currentChar)) {
         if (isalpha(currentChar)) {
             currentChar = tolower(currentChar);
@@ -24,18 +21,15 @@ int main() {
             if (prevChar != '\0') {
                 pairFrequency[prevChar - 'a'][currentChar - 'a']++;
             }
-
             prevChar = currentChar;
         }
     }
-
     cout << "Частота букв:" << endl;
     for (int i = 0; i < 26; i++) {
         if (letterFrequency[i] > 0) {
             cout << (char)(i + 'a') << ": " << letterFrequency[i] << endl;
         }
     }
-
     cout << "Частота двухбуквенных сочетаний:" << endl;
     for (int i = 0; i < 26; i++) {
         for (int j = 0; j < 26; j++) {
@@ -44,7 +38,6 @@ int main() {
             }
         }
     }
-
     file.close();
     return 0;
 }
